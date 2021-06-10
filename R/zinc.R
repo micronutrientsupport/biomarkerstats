7cf0f9564b8945a8ac87e2d8474e053d150f6725
Bean, Andrew K <andan@bsg.ac.uk>
2021-06-10 14:40:06 +0100

    chore: disable surveydesign

--- R/zinc.R
+++ R/zinc.R
@@ -28,8 +28,8 @@ BiomarkerData <- function(theData, biomarkerName, groupId, thresholds) {
   PhysLim<-300 # fixed figure until a csv to import
 
   #### Flags to accomodate differences between surveys
-  Flag_SurvWeightRun<-1 # where 1 = 'run' as not adjusted in the supplied data and 0 = 'do not run' as already adjusted
-  Flag_SurvWeightSUpplied<-1 # where not supplied set to 0, supplied = 1. Run if above = 1.
+  Flag_SurvWeightRun<-0 # where 1 = 'run' as not adjusted in the supplied data and 0 = 'do not run' as already adjusted
+  Flag_SurvWeightSUpplied<-0 # where not supplied set to 0, supplied = 1. Run if above = 1.
   Flag_HaemAltAdjust<-0  # Adjustment not run (0) where already adjusted in the supplied data, run if = 1.
   Flag_SmokeAdjust<-0 # Adjustment not run (0) where already adjusted in the supplied data, or smoking not recorded so cannot adjust
 
