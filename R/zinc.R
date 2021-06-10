5fe959421e1ec826b72867ce55135a22a5f3f7c1
Bean, Andrew K <andan@bsg.ac.uk>
2021-06-10 13:45:01 +0100

    feat: initial function headers:

--- R/zinc.R
+++ R/zinc.R
@@ -13,6 +13,34 @@
 #' @examples
 BiomarkerData <- function(theData, groupId, biomarkerName, thresholds) {
 
+  print(theData)
+
+  #### Bring in data ####
+  MyGp<-groupId  # change this to change the demographic group data used
+  MyMN<-10 # column for the biomarker data
+  MyAgg<-2 # this is the aggregate data field to be used
+  DataUse<-theData # this is the participant and biomarker data
+
+  #Make sure the biomarker column values are numeric
+  DataUse[,MyMN] = as.numeric(as.character(DataUse[,MyMN]))
+
+  #PhysLimits<-read.csv() # this is a table with physiological limit for each MN
+  PhysLim<-300 # fixed figure until a csv to import
+  # Thresholds
+  # thresholdUpper / thresholdLower come from the function call
+  #ThresholdUpper<-100 #Can have text, e.g. "No value" and script will function correctly
+  #ThresholdLower<-80 #
+
+  # Survey weight used to convert participant data to be representative of the wider pop'n
+  #SurveyWeight<-read.csv()
+
+  #### Flags to accomodate differences between surveys
+  Flag_SurvWeightRun<-1 # where 1 = 'run' as not adjusted in the supplied data and 0 = 'do not run' as already adjusted
+  Flag_SurvWeightSUpplied<-1 # where not supplied set to 0, supplied = 1. Run if above = 1.
+  Flag_HaemAltAdjust<-0  # Adjustment not run (0) where already adjusted in the supplied data, run if = 1.
+  Flag_SmokeAdjust<-0 # Adjustment not run (0) where already adjusted in the supplied data, or smoking not recorded so cannot adjust
+
+  print('Start!')
 
 }
 
