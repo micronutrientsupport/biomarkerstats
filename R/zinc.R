4b966c0d80b85c0d2304cbd501a44b51f6ef7095
Bean, Andrew K <andan@bsg.ac.uk>
2021-06-10 12:26:47 +0100

    feat: Add basic R package structure.  Following https://r-pkgs.org/whole-game.html

---
+++ R/zinc.R
@@ -0,0 +1,19 @@
+#' Biomarker Data
+#'
+#' Does some stuff
+#'
+#' @param theData the data
+#' @param groupId the group
+#' @param biomarkerName the biomarker
+#' @param thresholds the thresholds
+#'
+#' @return output
+#' @export
+#'
+#' @examples
+BiomarkerData <- function(theData, groupId, biomarkerName, thresholds) {
+
+
+}
+
+
